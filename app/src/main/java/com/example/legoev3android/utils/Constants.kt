0fc6b6d05181c2dccd61a0b8516970ca21e2e8ed
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-02-24 20:21:52 -0500

    Update Constants.kt

--- app/src/main/java/com/example/legoev3android/utils/Constants.kt
+++ app/src/main/java/com/example/legoev3android/utils/Constants.kt
@@ -2,6 +2,7 @@ package com.example.legoev3android.utils
 
 object Constants {
     const val REQUEST_CODE_BLUETOOTH_PERMISSION = 0
+    const val ROBOT_NAME = "EV3A"
     const val ROBOT_UUID = "PLACEHOLDER"
     const val STATE_NONE = 0 // Nothing
     const val STATE_LISTEN = 1 // Listening for incoming connection
