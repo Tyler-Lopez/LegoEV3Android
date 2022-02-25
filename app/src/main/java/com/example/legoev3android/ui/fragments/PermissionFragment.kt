3e4134eda845c06d97f593077f800118ddc1b111
Tyler Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2022-02-25 00:45:12 -0500

    Aesthetic refactor of logic to find devices

--- app/src/main/java/com/example/legoev3android/ui/fragments/PermissionFragment.kt
+++ app/src/main/java/com/example/legoev3android/ui/fragments/PermissionFragment.kt
@@ -12,18 +12,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.legoev3android.R
 import com.example.legoev3android.databinding.FragmentPermissionBinding
 import com.example.legoev3android.ui.viewmodels.MainViewModel
-import com.example.legoev3android.utils.BluetoothUtility
+import com.example.legoev3android.utils.PermissionUtility
 
 class PermissionFragment : Fragment(R.layout.fragment_permission) {
 
     private val viewModel: MainViewModel by viewModels()
     private var binding: FragmentPermissionBinding? = null
-    private var permissionMsg: TextView? = null
+
+    // When you only want to display a single message
+    private lateinit var centeredText: TextView
+
+    // When you want subtext and primary text
+    private lateinit var centerConstraintLayout: ConstraintLayout
+    private lateinit var textConstraintToSubtext: TextView
+    private lateinit var textConstraintToText: TextView
+
 
     // Used to launch and receive results for permission requests
     private val requestPermissionsLauncher =
@@ -31,10 +40,10 @@ class PermissionFragment : Fragment(R.layout.fragment_permission) {
             ActivityResultContracts.RequestMultiplePermissions()
         ) { isGranted ->
             if (isGranted.containsValue(false)) {
-                permissionMsg?.text = "At least one permission was denied."
+                textConstraintToSubtext.text = "Permissions are required for this app to function."
             } else {
                 // Permissions were granted
-                startBluetoothConnection()
+                findAvailableDevices()
             }
         }
 
@@ -42,14 +51,14 @@ class PermissionFragment : Fragment(R.layout.fragment_permission) {
     // Including those not yet paired
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            println("here")
             val action: String? = intent.action
             // If a device was found
             if (action == BluetoothDevice.ACTION_FOUND) {
                 val device: BluetoothDevice? =
                     intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                 device?.run {
-                    permissionMsg?.text = permissionMsg?.text.toString() + "\n" + device.address + device.name + device.uuids
+                    centeredText.text =
+                       centeredText.text.toString() + "\n" + device.address + device.name + device.uuids
                 }
             }
         }
@@ -66,14 +75,25 @@ class PermissionFragment : Fragment(R.layout.fragment_permission) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPermissionBinding.bind(view)
-        permissionMsg = binding?.permissionMsg
+
+        textConstraintToSubtext = binding!!.permissionText
+        textConstraintToText = binding!!.permissionSubtext
+        centeredText = binding!!.centeredText
+        centerConstraintLayout = binding!!.constrainLayoutCenter
+
+        centeredText.text = "LOADING BLUETOOTH"
 
         // If the user already has given bluetooth permissions
-        if (false)// COME BACK TO LATER DEBUGGING ISSUE
-   //     if (BluetoothUtility.hasBluetoothPermissions(requireContext())) {
-            startBluetoothConnection()
+        if (PermissionUtility.hasPermissions(requireContext()))
+            findAvailableDevices()
         else {
-            binding?.permissionMsg?.setOnClickListener {
+            // Else, make text and subtext visible
+                //
+            centeredText.visibility = View.GONE
+            centerConstraintLayout.visibility = View.VISIBLE
+            textConstraintToSubtext.text = "This application requires Location and Bluetooth permissions."
+            textConstraintToText.text = "Click to grant permissions."
+            centerConstraintLayout.setOnClickListener {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                     requestPermissionsLauncher.launch(
                         arrayOf(
@@ -101,18 +121,24 @@ class PermissionFragment : Fragment(R.layout.fragment_permission) {
         binding = null
     }
 
-    fun startBluetoothConnection() {
+    private fun findAvailableDevices() {
         val adapter =
             (requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager)
                 .adapter
-        if (adapter == null)
-        // Device doesn't support bluetooth
-            permissionMsg?.text = "Device doesn't support Bluetooth."
+
+        centerConstraintLayout.visibility = View.GONE
+
+        if (adapter == null) {
+            // Hide the centerConstraintLayout, make centered text visible
+            centeredText.visibility = View.VISIBLE
+            centeredText.text = "This device does not support Bluetooth connections."
+        }
         else {
             // Device supports bluetooth
             // Print out all discoverable devices
-            permissionMsg?.text = "..."
-            println("Attempting to start discovery")
+            centeredText.visibility = View.VISIBLE
+            centeredText.text = "Finding devices"
+            // Add recycler view here soon
             println(adapter.startDiscovery())
         }
     }
---
+++ app/src/main/java/com/example/legoev3android/utils/PermissionUtility.kt
@@ -0,0 +1,46 @@
+package com.example.legoev3android.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.content.ContextCompat
+
+object PermissionUtility {
+    fun hasPermissions(context: Context) =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_SCAN,
+            ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.BLUETOOTH_SCAN,
+                    ) == PackageManager.PERMISSION_GRANTED
+                    &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                    ) == PackageManager.PERMISSION_GRANTED
+                    &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                    ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH,
+            ) == PackageManager.PERMISSION_GRANTED
+                    &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                    ) == PackageManager.PERMISSION_GRANTED
+                    &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                    ) == PackageManager.PERMISSION_GRANTED
+        }
+}
